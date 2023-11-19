5199a2b22aee00d71c9da3991d89630b8b2d02b9
hadi499 <hadi.ygcl@gmail.com>
2023-11-19 13:25:14 +0700

    update

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Traits\HasCreatorAndUpdater;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
 
 class File extends Model
 {
-    use HasFactory;
+    use HasFactory, NodeTrait, HasCreatorAndUpdater;
 }
