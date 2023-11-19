cc014b6f744e9f8d575728944e0de69913d382f8
hadi499 <hadi.ygcl@gmail.com>
2023-11-19 08:13:45 +0700

    ui

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-[600px] h-[80px] flex items-center">
+        <TextInput
+            type="text"
+            class="block w-full mr-2"
+            autocomplete
+            placeholder="Search for files and folders"
+        />
+    </div>
+</template>
+
+<script setup>
+import TextInput from '@/Components/TextInput.vue'
+</script>
